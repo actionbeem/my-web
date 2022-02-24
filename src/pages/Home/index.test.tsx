bd022622677ebdd487fc96ed9f0b24447c63a206
김종혁 <actionbeem929@gmail.com>
2022-02-24 15:53:37 +0900

    [ ci/cd 테스트 ] 테스트 코드 성공시

--- src/pages/Home/index.test.tsx
+++ src/pages/Home/index.test.tsx
@@ -3,6 +3,6 @@ import Home from ".";
 
 test("renders learn react link", () => {
   render(<Home />);
-  const testElement = screen.getByText(/test action/i);
+  const testElement = screen.getByText(/test/i);
   expect(testElement).toBeInTheDocument();
 });
